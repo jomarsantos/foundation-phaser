9044604fa55829d65c63396e245bdcf8cb14e262
Jomar Santos <jomaroliversantos@gmail.com>
2018-04-17 20:03:44 -0700

    static resources

--- server.js
+++ server.js
@@ -5,13 +5,14 @@ const config = configFile[env];
 
 // Init express app
 const express = require('express');
+const path = require("path");
 const app = express();
 
 // Routes
 app.get('/',function(req, res) {
-	res.sendFile(__dirname + '/client/index.html');
+	res.sendFile(path.join(__dirname, '/client/index.html'));
 });
-app.use('/client', express.static(__dirname + '/client'));
+app.use('/', express.static(path.join(__dirname, '/client')));
 
 // Start server
 const server = app.listen(config.port, function() {
